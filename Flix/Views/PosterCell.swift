5f5da632259bbdcce310a8d7d3afcd8a127be5b4
DennyHo2020 <dennyho@email.arizona.edu>
2018-09-22 22:25:01 -0700

    done

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Denny Ho on 9/22/18.
+//  Copyright © 2018 Denny Ho. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
